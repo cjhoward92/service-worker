ae5e12ee4c999720e0cdb027bd90327d4c7722e4
Carson Howard <choward@aspen-systems.com>
2019-02-11 09:54:12 -0700

    Cache and fetch requests

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+const CACHE_NAME = 'service_worker_cache_test';
+const urlsToCache = [
+  '/index.html',
+  '/install.js',
+  'https://fonts.google.com/?selection.family=Roboto',
+];
+
 self.addEventListener('install', (event) => {
   console.log('installed');
+
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+  }));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request)
+    .then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }));
 });
